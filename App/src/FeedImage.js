8cc3c80132ff5803c6618429b2d411b26b4b22b7
Henry Goddard <godd9170@mylaurier.ca>
2016-01-23 17:24:49 -0500

    icons added

--- App/src/FeedImage.js
+++ App/src/FeedImage.js
@@ -8,16 +8,19 @@ import React, {
   Text
 } from 'react-native';
 
+var Icon = require('react-native-vector-icons/Ionicons');
+
 
 export default class FeedImage extends Component {
 
   render() {
     const image = this.props.image;
-    const url = `https://picsule.herokuapp.com${image}`;
+    const url = `https://picsule.herokuapp.com${image.url}`;
     console.log("URL", url);
     return (
       <View>
         <Image source={{uri: url}} style={styles.feedImage} />
+        <Icon name="arrow-graph-up-right" size={30} color="#900" />
       </View>
     );
   }
