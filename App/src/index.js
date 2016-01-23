20faf8650261f0ea38408772eb8fe11797c3630e
Henry Goddard <godd9170@mylaurier.ca>
2016-01-23 18:36:04 -0500

    lose George's GD justify styles

--- App/src/index.js
+++ App/src/index.js
@@ -84,9 +84,7 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flex: 1
   },
   welcome: {
     fontSize: 20,
