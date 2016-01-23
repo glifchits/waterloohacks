db56476ccb5ed00d5c7090c2b61b8c6b7e55c801
Henry Goddard <godd9170@mylaurier.ca>
2016-01-23 02:57:23 -0500

    webpack jsx try

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var App = React.createClass({
+
+  render: function() {
+    return (
+      <div>Hello</div>
+    );
+  }
+
+});
+
+React.render(<App />, document.getElementById('app'));
