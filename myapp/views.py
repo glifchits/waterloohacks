e473618c6ab56c81d9ab4f637e60966eed2d55f5
William Ma <williamjma@hotmail.com>
2016-01-23 16:43:55 -0500

    added picture metadata to json

--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,8 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
+import Image
+from PIL.ExifTags import TAGS, GPSTAGS
 import json
 from myapp.models import Document
 from myapp.forms import DocumentForm
@@ -39,6 +41,78 @@ def getImages(request):
     # Load documents for the list page
     documents = Document.objects.all()
     # Render list page with the documents and the form
-    list = [d.docfile.url for d in documents]
+
+    #for d in documents:
+    list = [];
+    for d in documents:
+        ret = {}
+        try:
+            imageObj = Image.open('myproject' + d.docfile.url)#('myproject' + d.docfile.url)#'C:\\Users\\William\\Downloads\\IMG_2301.JPG')
+            info = imageObj._getexif()
+            if info is not None:
+                for tag, value in info.items():
+                    decoded = TAGS.get(tag, tag)
+                    if decoded == "GPSInfo":
+                        gps_data = {}
+                        for t in value:
+                            sub_decoded = GPSTAGS.get(t, t)
+                            gps_data[sub_decoded] = value[t]
+                        lat,long = get_lat_lon(gps_data)
+                        ret["Latitude"] = lat;
+                        ret["Longitude"] = long;
+                    else:
+                        ret[decoded] = value
+        except:
+            pass
+        list.append({"url:": d.url, "Model": ret.get("Model"), "Make": ret.get("Make"),
+                      "Orientation": ret.get("Orientation"), "Date": ret.get("DateTime"),
+                      "Width": ret.get("ExifImageWidth"), "Height": ret.get("ExifImageHeight"),
+                      "Latitude": ret.get("Latitude"), "Longitude": ret.get("Longitude")})
+
     return HttpResponse(json.dumps(list), content_type="application/json")
 
+
+def _get_if_exist(data, key):
+    if key in data:
+        return data[key]
+
+    return None
+
+
+def _convert_to_degress(value):
+    """Helper function to convert the GPS coordinates stored in the EXIF to degress in float format"""
+    d0 = value[0][0]
+    d1 = value[0][1]
+    d = float(d0) / float(d1)
+
+    m0 = value[1][0]
+    m1 = value[1][1]
+    m = float(m0) / float(m1)
+
+    s0 = value[2][0]
+    s1 = value[2][1]
+    s = float(s0) / float(s1)
+
+    return d + (m / 60.0) + (s / 3600.0)
+
+
+def get_lat_lon(gps_info):
+    """Returns the latitude and longitude, if available, from the provided exif_data (obtained through get_exif_data above)"""
+    lat = None
+    lon = None
+
+    gps_latitude = _get_if_exist(gps_info, "GPSLatitude")
+    gps_latitude_ref = _get_if_exist(gps_info, 'GPSLatitudeRef')
+    gps_longitude = _get_if_exist(gps_info, 'GPSLongitude')
+    gps_longitude_ref = _get_if_exist(gps_info, 'GPSLongitudeRef')
+
+    if gps_latitude and gps_latitude_ref and gps_longitude and gps_longitude_ref:
+        lat = _convert_to_degress(gps_latitude)
+        if gps_latitude_ref != "N":
+            lat = 0 - lat
+
+        lon = _convert_to_degress(gps_longitude)
+        if gps_longitude_ref != "E":
+            lon = 0 - lon
+
+    return lat, lon
